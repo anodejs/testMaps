e2a2dc635dd160ef3769a8995d19af573da2f839
amit <amachluf@hotmail.com>
2014-08-09 20:08:28 +0300

    style main

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,129 +1,53 @@
 package com.example.administrator.myapplication;
+import java.util.ArrayList;
 
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ListView;
 
+public class MainActivity extends ListActivity {
 
-public class MainActivity extends FragmentActivity {
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
 
-    // When requested, this adapter returns a DemoObjectFragment,
-    // representing an object in the collection.
-    //DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
-    //ViewPager mViewPager;
-    ActionBar actionBar;
+        // if extending Activity
+        //setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // 1. pass context and data to the custom adapter
+        MyAdapter adapter = new MyAdapter(this, generateData());
 
-        Button findNearMeButton = (Button)findViewById(R.id.findnearme);
-        findNearMeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, NearMeNow.class);
-                startActivity(myIntent);
-            }
-        });
+        // if extending Activity 2. Get ListView from activity_main.xml
+        //ListView listView = (ListView) findViewById(R.id.listview);
 
-        /*
-        actionBar = getActionBar();
+        // 3. setListAdapter
+        //listView.setAdapter(adapter); if extending Activity
+        setListAdapter(adapter);
+    }
 
-        // Specify that tabs should be displayed in the action bar.
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+    private ArrayList<Model> generateData(){
+        ArrayList<Model> models = new ArrayList<Model>();
+        models.add(new Model("Group Title"));
+        models.add(new Model(R.drawable.icon,"מצא עכשיו"));
+        models.add(new Model(R.drawable.icon,"לוח תפילות"));
+        models.add(new Model(R.drawable.icon,"תוספות"));
 
-        // Create a tab listener that is called when the user changes tabs.
-        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-                    // When the given tab is selected, show the tab contents in the
-                    // container view.
-                    Fragment fragment = new DemoObjectFragment();
-                    Bundle args = new Bundle();
-                    args.putInt(DemoObjectFragment.ARG_OBJECT,
-                            tab.getPosition() + 1);
-                    fragment.setArguments(args);
-                    getFragmentManager().beginTransaction()
-                            .replace(R.id.container, fragment).commit();
-            }
-
-
-
-            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-                // hide the given tab
-            }
-
-            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-                // probably ignore this event
-            }
-        };
-
-        // Add 3 tabs, specifying the tab's text and TabListener
-        for (int i = 0; i < 3; i++) {
-            actionBar.addTab(
-                    actionBar.newTab()
-                            .setText("Tab " + (i + 1))
-                            .setTabListener(tabListener));
-        }
-
-
-        // ViewPager and its adapters use support library
-        // fragments, so use getSupportFragmentManager.
-        /*
-        mDemoCollectionPagerAdapter =
-                new DemoCollectionPagerAdapter(
-                        getSupportFragmentManager());
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mDemoCollectionPagerAdapter);
-
-        */
+        return models;
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    // Instances of this class are fragments representing a single
-// object in our collection.
-    public static class DemoObjectFragment extends Fragment {
-        public static final String ARG_OBJECT = "object";
-
-        @Override
-        public View onCreateView(LayoutInflater inflater,
-                                 ViewGroup container, Bundle savedInstanceState) {
-            // The last two arguments ensure LayoutParams are inflated
-            // properly.
-            View rootView = inflater.inflate(
-                    R.layout.fragment_collection_object, container, false);
-            Bundle args = getArguments();
-            ((TextView) rootView.findViewById(android.R.id.text1)).setText(
-                    Integer.toString(args.getInt(ARG_OBJECT)));
-            return rootView;
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        //Place item = (Place) m_adapter.getItem(position);
+        if (position == 1) {
+            Intent intent = new Intent(MainActivity.this, NearMeNow.class);
+         //ADD MY  LOCATION
+            //Bundle b = new Bundle();
+            //b.putString("address", item.getAddress());
+           // intent.putExtras(b);
+            startActivity(intent);
         }
     }
+
 }
