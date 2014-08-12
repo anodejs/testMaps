154bcfc144d389fab232ec350c022c406b55ebdc
amit <amachluf@hotmail.com>
2014-08-12 22:05:25 +0300

    add sidor

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.nfc.tech.NfcBarcode;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
+
+    GPSTracker gps;
 
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -32,7 +36,7 @@ public class MainActivity extends ListActivity {
         models.add(new Model("Group Title"));
         models.add(new Model(R.drawable.icon,"מצא עכשיו"));
         models.add(new Model(R.drawable.icon,"לוח תפילות"));
-        models.add(new Model(R.drawable.icon,"תוספות"));
+        models.add(new Model(R.drawable.icon,"סידור"));
 
         return models;
     }
@@ -40,14 +44,37 @@ public class MainActivity extends ListActivity {
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         //Place item = (Place) m_adapter.getItem(position);
+        //choose nearMeNow
         if (position == 1) {
-            Intent intent = new Intent(MainActivity.this, NearMeNow.class);
-         //ADD MY  LOCATION
-            //Bundle b = new Bundle();
-            //b.putString("address", item.getAddress());
-           // intent.putExtras(b);
-            startActivity(intent);
+            gps = new GPSTracker(this);
+
+            if (gps.canGetLocation()) {
+
+                Toast.makeText(getApplicationContext(), gps.getLatitude() + "", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MainActivity.this, NearMeNow.class);
+
+                //ADD MY  LOCATION
+                Bundle b = new Bundle();
+                b.putDouble("Longitude", gps.getLatitude());
+                b.putDouble("Latitude", gps.getLatitude());
+                //b.putString("address", item.getAddress());
+                intent.putExtras(b);
+                startActivity(intent);
+            } else {
+                gps.showSettingsAlert();
+            }
         }
-    }
+
+        //choose sidor
+            else if (position == 3)
+            {
+                Intent intent = new Intent(MainActivity.this, SidorActivity.class);
+                startActivity(intent);
+
+            }
+
+        }
+
 
 }
---
+++ app/src/main/java/com/example/administrator/myapplication/SidorActivity.java
@@ -0,0 +1,66 @@
+package com.example.administrator.myapplication;
+
+    import android.app.Activity;
+    import android.os.Bundle;
+    import android.text.method.ScrollingMovementMethod;
+    import android.view.View;
+    import android.view.View.OnClickListener;
+    import android.widget.Button;
+    import android.widget.ScrollView;
+    import android.widget.Spinner;
+    import android.widget.TextView;
+    import android.widget.Toast;
+
+
+    public class SidorActivity extends Activity {
+
+        private Spinner spinnerType, spinnerVerison;
+        private Button btnSubmit;
+        private TextView sidorText;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_sidor);
+
+            sidorText =(TextView) findViewById(R.id.TextViewSidor);
+            sidorText.setMovementMethod(new ScrollingMovementMethod());
+
+            addListenerOnButton();
+            addListenerOnSpinnerItemSelection();
+        }
+
+
+
+        public void addListenerOnSpinnerItemSelection() {
+            spinnerType = (Spinner) findViewById(R.id.spinnerType1);
+            spinnerType.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+
+            spinnerVerison = (Spinner) findViewById(R.id.spinnerVerison1);
+            spinnerVerison.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        }
+
+        // get the selected dropdown list value
+        public void addListenerOnButton() {
+
+            spinnerType = (Spinner) findViewById(R.id.spinnerType1);
+            spinnerVerison = (Spinner) findViewById(R.id.spinnerVerison1);
+            btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+            btnSubmit.setOnClickListener(new OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    //send and get from the server
+
+                    Toast.makeText(getApplicationContext(),
+                            "OnClickListener : " +
+                                    "\nSpinner 1 : "+ String.valueOf(spinnerType.getSelectedItem()) +
+                                    "\nSpinner 2 : "+ String.valueOf(spinnerVerison.getSelectedItem()),
+                            Toast.LENGTH_SHORT).show();
+
+                }
+
+            });
+        }
+    }
